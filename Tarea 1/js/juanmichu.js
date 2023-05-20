d056b0ef10fdb87ac4f07aafe770ff2131dd1a9b
Juan Manuel Serrano Pérez <40150097+Juanmichu@users.noreply.github.com>
2023-05-20 15:59:44 +0200

    Add files via upload

---
+++ Tarea 1/js/juanmichu.js
@@ -0,0 +1,4 @@
+setTimeout(() => {
+    window.alert('Hola soy JUANMICHU desde un archivo externo');
+},1000)
+//Así podremos ver el mensaje un segundo después de que la web cargue, pudiendo ver el h1 insertado en el body
