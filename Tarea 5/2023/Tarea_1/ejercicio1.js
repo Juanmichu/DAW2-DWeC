ebe941f09bfe6d301a98c9eea79a903d7b0190f0
Juanmichu <juanmichu@gmail.com>
2023-06-03 20:43:57 +0200

    Added exercise 6 to Tarea 5 > 2023 > Tarea_1

--- Tarea 5/2023/Tarea_1/ejercicio1.js
+++ Tarea 5/2023/Tarea_1/ejercicio1.js
@@ -22,14 +22,18 @@ art6.setAttribute('style','display: none;');
 // 4. Numeramos los artículos.
 for(let key = 0; key < articles.length; key++) {
     // Obtenemos el primer div de cada artículo que corresponde con la cabecera del artículo. Luego, insertamos el número al principio.
-    let articleTextDiv1 = articles[key].getElementsByTagName('div')[0];
+    let articleTextDivs = articles[key].getElementsByTagName('div');
+    let articleTextDiv1 = articleTextDivs[0];
     articleTextDiv1.innerHTML = (key + 1) + ' - ' + articleTextDiv1.innerHTML;
 
     // 5. Añadir la clase cabecera al primer <div> de cada elemento <article>.
     // Aprovecho el bucle for para el ejercicio 5.
     articleTextDiv1.setAttribute('class','cabecera');
-    // 6. Reemplazar Servicio Murciano de Salud por S.M.S.
-    //document.body.innerHTML = document.body.innerHTML.replace('/Servicio Murciano de Salud/g', 'S.M.S');
+
+    // 6. Reemplazar Servicio Murciano de Salud por S.M.S en los textos que haya dentro de cada <article>
+    Object.values(articleTextDivs).forEach((element) => {
+        element.innerHTML = element.innerHTML.replace(/Servicio Murciano de Salud/gi, 'S.M.S');
+    });
 
 }
 
