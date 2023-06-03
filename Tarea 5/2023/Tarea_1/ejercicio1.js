9aef1b8b9d9e7e3352d2a293f24d66bb95e8bd42
juanmichu <juanmichu@gmail.com>
2023-06-03 14:09:14 +0200

    Added 2023 Tarea 5. Remains to change Servicio Murciano Salud into SMS

---
+++ Tarea 5/2023/Tarea_1/ejercicio1.js
@@ -0,0 +1,36 @@
+// 1. Modificar título.
+let titulo = document.getElementById('titulo');
+    titulo.innerHTML = 'Juan Manuel Serrano Pérez';
+
+// 2. Modificar imagen artículo 2.
+let noticias = document.getElementById('noticias');
+let artImages = noticias.getElementsByTagName('img');
+
+// Aquí recogemos el elemento que nos interesa cambiar. Hay que recordar que queremos la 2º imagen.
+let art2Image = artImages[1];
+// Seteamos la nueva imagen.
+art2Image.setAttribute('src', 'New_article2_img.jpg');
+
+// 3. Oculta el último artículo.
+// Retomamos el elemento con ID "noticias" para poder tomar los nodos hijo que nos interesan.
+const articles = noticias.getElementsByTagName('article');
+let art6 = articles[5];
+
+// Ocultamos este artículo. Seteamos el display: none para ocultar. No se dice nada de eliminar.
+art6.setAttribute('style','display: none;');
+
+// 4. Numeramos los artículos.
+for(let key = 0; key < articles.length; key++) {
+    // Obtenemos el primer div de cada artículo que corresponde con la cabecera del artículo. Luego, insertamos el número al principio.
+    let articleTextDiv1 = articles[key].getElementsByTagName('div')[0];
+    articleTextDiv1.innerHTML = (key + 1) + ' - ' + articleTextDiv1.innerHTML;
+
+    // 5. Añadir la clase cabecera al primer <div> de cada elemento <article>.
+    // Aprovecho el bucle for para el ejercicio 5.
+    articleTextDiv1.setAttribute('class','cabecera');
+    // 6. Reemplazar Servicio Murciano de Salud por S.M.S.
+    //document.body.innerHTML = document.body.innerHTML.replace('/Servicio Murciano de Salud/g', 'S.M.S');
+
+}
+
+
