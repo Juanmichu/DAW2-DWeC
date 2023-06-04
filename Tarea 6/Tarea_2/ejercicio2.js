a5369e1c725990b3d39e349eb1e281ca4bcb8375
juanmichu <juanmichu@gmail.com>
2023-06-04 23:03:00 +0200

    Ended Tarea 6 > Tarea_2

--- Tarea 6/Tarea_2/ejercicio2.js
+++ Tarea 6/Tarea_2/ejercicio2.js
@@ -20,8 +20,12 @@ let inputClave = document.createElement('input');
 let fieldNumeros = document.createElement('fieldset');
     fieldNumeros.id = 'field_numeros';
 
+let respuesta = document.createElement('div');
+    respuesta.setAttribute('id', 'respuesta');
+
 // Insertamos los elementos en el DOM.
 document.body.appendChild(panelNumeros);
+document.body.appendChild(respuesta);
 panelNumeros.appendChild(formPanel);
 formPanel.appendChild(inputClave);
 formPanel.appendChild(fieldNumeros);
@@ -38,9 +42,16 @@ for(let i = 1; i <= 9; i++) {
         numeros.push(numero);
 }
 
-// Añadimos el teclado de forma aleatoria.
+// Añadimos el teclado de forma aleatoria y enlazamos un eventListener a cada botón.
 for(let i = 9; i > 0; i--) {
     let randomNumber = Math.floor(Math.random() * i);
+
+    // Añadimos un evento para cada botón creado. Introducirá el número tecleado en el panel y borrará cualquier mensaje mostrado.
+    numeros[randomNumber].addEventListener('click', (event) => {
+       inputClave.value += event.target.value;
+       respuesta.classList.remove('show');
+    });
+
     fieldNumeros.appendChild(numeros[randomNumber]);
     numeros.splice(randomNumber, 1);
 }
@@ -51,9 +62,39 @@ let botonBorrar = document.createElement('button');
 
 let botonValidar = document.createElement('button');
     botonValidar.id         = 'validar';
-    botonValidar.type       = 'submit';
     botonValidar.innerHTML  = 'VALIDAR';
 
 fieldNumeros.appendChild(botonBorrar);
 fieldNumeros.appendChild(botonValidar);
+
+// FUNCIONALIDAD BORRAR CODIGO.
+botonBorrar.addEventListener('click', (event) => {
+    event.preventDefault();
+    respuesta.classList.remove('show');
+    inputClave.value = inputClave.value.slice(0, -1);
+});
+
+// FUNCIONALIDAD VALIDAR.
+botonValidar.addEventListener('click', (event) => {
+    // Evitamos el submit de ese botón. Queremos que simplemente valide y simule el POST al servidor.
+    event.preventDefault();
+
+    let claveValor  = formPanel.clave.value;
+    let claveRegExp = /^[0-9]{4}$/g;
+    let claveCorrecta = /9999/g
+
+    if(!claveValor.match(claveRegExp)) {
+        respuesta.setAttribute('class','error');
+        respuesta.innerHTML = 'El código introducido tiene un formato incorrecto';
+    } else if (!claveValor.match(claveCorrecta)) {
+        respuesta.setAttribute('class', 'error');
+        respuesta.innerHTML = 'El código introducido es incorrecto';
+    } else {
+        respuesta.setAttribute('class', 'exito');
+        respuesta.innerHTML = '¡Código correcto!';
+    }
+    if(!respuesta.classList.contains('show')) {
+        respuesta.classList.add('show');
+    }
+});
 
